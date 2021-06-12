deadf7bba589c5799dfa08ce7d6250b3df467a05
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-06-11 23:52:35 -0400

    downloaded Font Awesome, started import to components

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faPlay,  } from '@fortawesome/free-solid-svg-icons';
+
 
 const Player = () => {
     return (

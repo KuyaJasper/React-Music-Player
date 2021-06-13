323b501cfe626d8dc6acb32bcf99e3403f63aa3b
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-06-13 17:24:39 -0400

    set up useRef in player.js

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useRef} from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPlay,
@@ -6,7 +6,16 @@ import {
   faAngleRight,
 } from "@fortawesome/free-solid-svg-icons";
 
-const Player = () => {
+const Player = ({currentSong}) => {
+//Ref (this is used to connect the HTML element like a querySelector from JavaScrip)
+const audioRef =useRef(null)
+
+
+//Event Handlers
+const playSongHandler = () =>{
+  console.log(audioRef.current);
+}
+
   return (
     <div className="player">
       <div className="time-control">
@@ -16,13 +25,14 @@ const Player = () => {
       </div>
       <div className="play-control">
         <FontAwesomeIcon className="skip-back" icon={faAngleLeft} size="2x" />
-        <FontAwesomeIcon className="play" icon={faPlay} size="2x" />
+        <FontAwesomeIcon onClick={playSongHandler} className="play" icon={faPlay} size="2x" />
         <FontAwesomeIcon
           className="skip-forward"
           icon={faAngleRight}
           size="2x"
         />
       </div>
+      <audio ref={audioRef} src={currentSong.audio}></audio>
     </div>
   );
 };

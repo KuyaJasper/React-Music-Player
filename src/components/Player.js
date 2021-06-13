e62c7d60fbcca06c9459f1cc37c3225668a9bdf9
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-06-13 16:27:28 -0400

    set up initial stylings for player controls and song

--- src/components/Player.js
+++ src/components/Player.js
@@ -14,7 +14,7 @@ const Player = () => {
         <input type="range" />
         <p>End Time</p>
       </div>
-      <div className="playcontrol">
+      <div className="play-control">
         <FontAwesomeIcon className="skip-back" icon={faAngleLeft} size="2x" />
         <FontAwesomeIcon className="play" icon={faPlay} size="2x" />
         <FontAwesomeIcon

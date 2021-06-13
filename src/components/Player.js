c95a7ce65a38e1e259121567120ebbb5905af990
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-06-13 18:13:15 -0400

    updated song button to toggle icons with a turnerary operator

--- src/components/Player.js
+++ src/components/Player.js
@@ -1,67 +1,91 @@
-import React, {useRef, useState} from "react";
+import React, { useRef, useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
   faPlay,
   faAngleLeft,
   faAngleRight,
+  faPause,
 } from "@fortawesome/free-solid-svg-icons";
 
-const Player = ({currentSong, isPlaying, setIsPlaying}) => {
-//Ref <======== (this is used to connect the HTML element like a querySelector from JavaScrip)
-const audioRef =useRef(null)
+const Player = ({ currentSong, isPlaying, setIsPlaying }) => {
+  //Ref <======== (this is used to connect the HTML element like a querySelector from JavaScrip)
+  const audioRef = useRef(null);
 
-//Event Handlers
-const playSongHandler = () =>{
-  // console.log(audioRef.current); <=== best practice to see if useRef is working as intended
+  //Event Handlers
+  const playSongHandler = () => {
+    // console.log(audioRef.current); <=== best practice to see if useRef is working as intended
 
-  if(isPlaying){
-     // .pause/ .play  method is built into the audio tag.
-     // setIsPlaying(!isPlaying) is a true/false function using the states passed down as props
+    if (isPlaying) {
+      // .pause/ .play  method is built into the audio tag.
+      // setIsPlaying(!isPlaying) is a true/false function using the states passed down as props
       audioRef.current.pause();
       setIsPlaying(!isPlaying);
-  }else{
-    audioRef.current.play();
-    setIsPlaying(!isPlaying);
-  }
-};
+    } else {
+      audioRef.current.play();
+      setIsPlaying(!isPlaying);
+    }
+  };
 
-const timeHandler = (event) =>{
-const current = event.target.currentTime;
-const duration = event.target.duration;
-setSongInfo({...songInfo, currentTime: current, duration});
-};
+  const timeHandler = (event) => {
+    const current = event.target.currentTime;
+    const duration = event.target.duration;
+    setSongInfo({ ...songInfo, currentTime: current, duration });
+  };
 
-const getTime= (time) => {
-  return(
-    //formats time into seconds and minutes
-    Math.floor(time / 60) + ":" + ("0" + Math.floor(time%60)).slice(-2)
-  );
-};
+  const getTime = (time) => {
+    return (
+      //formats time into seconds and minutes
+      Math.floor(time / 60) + ":" + ("0" + Math.floor(time % 60)).slice(-2)
+    );
+  };
 
-//State
-const [songInfo,setSongInfo] = useState({
-  currentTime: null,
-  duration: null,
-})
+  const dragHandler = (event) => {
+    //onChange is an input method that tracks the change
+    audioRef.current.currentTime = event.target.value;
+    setSongInfo({ ...songInfo, currentTime: event.target.value });
+  };
+
+  //State
+  const [songInfo, setSongInfo] = useState({
+    currentTime: 0,
+    duration: 0,
+  });
 
   return (
     <div className="player">
       <div className="time-control">
         <p>{getTime(songInfo.currentTime)}</p>
-        <input type="range" />
+        <input
+          min={0}
+          max={songInfo.duration}
+          value={songInfo.currentTime}
+          type="range"
+          onChange={dragHandler}
+        />
         <p>{getTime(songInfo.duration)}</p>
       </div>
       <div className="play-control">
         <FontAwesomeIcon className="skip-back" icon={faAngleLeft} size="2x" />
-        <FontAwesomeIcon onClick={playSongHandler} className="play" icon={faPlay} size="2x" />
+        <FontAwesomeIcon
+          onClick={playSongHandler}
+          className="play"
+          //? and : is a turnerary operator to invoke the icons
+          icon={isPlaying ? faPause : faPlay}
+          size="2x"
+        />
         <FontAwesomeIcon
           className="skip-forward"
           icon={faAngleRight}
           size="2x"
         />
       </div>
-      {/* onTimeupdate is uniqure to audio like onCLick */}
-      <audio onTimeUpdate={timeHandler} onLoadedMetadata={timeHandler} ref={audioRef} src={currentSong.audio}></audio>
+      <audio
+      //onTimeUpdate and onLoadedMetadata are unique to audio tags, can be used like onClick method
+        onTimeUpdate={timeHandler}
+        onLoadedMetadata={timeHandler}
+        ref={audioRef}
+        src={currentSong.audio}
+      ></audio>
     </div>
   );
 };

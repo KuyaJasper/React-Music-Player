2202cd05cba9926bce9acb5cab6cdebf8293e8a4
Jasper Abarquez <78329142+KuyaJasper@users.noreply.github.com>
2021-06-17 17:29:36 -0400

    added new songs

--- src/data.js
+++ src/data.js
@@ -14,6 +14,15 @@ function chillHop() {
       audio: "https://mp3.chillhop.com/serve.php/?mp3=8200",
     },
     {
+      name: "Bliss",
+      artist: "Misha, Jussi Halme",
+      cover: "https://chillhop.com/wp-content/uploads/2020/09/5bff1a6f1bd0e2168d29b4c841b811598135e457.jpg",
+      id: uuidv4(),
+      active: false,
+      color: ["#2A416D","#EB8086"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=9248",
+    },
+    {
       name: "Beaver Creek",
       artist: "Aso, Middle School, Aviino",
       cover: "https://chillhop.com/wp-content/uploads/2020/09/0255e8b8c74c90d4a27c594b3452b2daafae608d.jpg",
@@ -31,6 +40,26 @@ function chillHop() {
       active: true,
       color: ["#E87274", "#2D385A"],
       audio: "https://mp3.chillhop.com/serve.php/?mp3=15088",
+    },
+    {
+      name: "Clocks Forward",
+      artist: "Psalm Trees, Guillaume Muschalle",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/db956cc3e3bb2d3725d0ce3f9aaf7190671c9451.jpg",
+      id: uuidv4(),
+      active: true,
+      color: ["#BB7970", "#F8D09F"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=8094",
+    },
+    {
+      name: "Far From Home",
+      artist: "Toonorth",
+      cover:
+        "https://chillhop.com/wp-content/uploads/2020/07/ad7fc4dda66ba986466fd2b1c416b0b12411ee28.jpg",
+      id: uuidv4(),
+      active: true,
+      color: ["#537879", "#F5E4B7"],
+      audio: "https://mp3.chillhop.com/serve.php/?mp3=8127",
     },
     {
       name: "Arcade",
